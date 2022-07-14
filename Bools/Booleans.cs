a5c99d245416bb43267ac849e71cce5cb10d95c7
Inghv <90041989+Inghv@users.noreply.github.com>
2022-07-14 18:26:37 +0300

    Bools

---
+++ Bools/Booleans.cs
@@ -0,0 +1,17 @@
+﻿namespace Bools
+{
+    public static class Booleans
+    {
+        public static bool ReturnTrue()
+        {
+            return true;
+            throw new NotImplementedException();
+        }
+
+        public static bool ReturnFalse()
+        {
+            return false;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bools/LogicalPuzzles.cs
@@ -0,0 +1,47 @@
+﻿namespace Bools
+{
+    public static class LogicalPuzzles
+    {
+        public static bool Puzzle1(bool b1, bool b2)
+        {
+            return !b1 || b2;
+            throw new NotImplementedException();
+        }
+
+        public static bool Puzzle2(bool b1, bool b2)
+        {
+            return !b1 && b2;
+            throw new NotImplementedException();
+        }
+
+        public static bool Puzzle3(bool b1, bool b2)
+        {
+            return !(!b1 && b2);
+            throw new NotImplementedException();
+        }
+
+        public static bool Puzzle4(bool b1, bool b2)
+        {
+            return b1 && !b2;
+            throw new NotImplementedException();
+        }
+
+        public static bool Puzzle5(bool b1, bool b2)
+        {
+            return !(b1 || b2);
+            throw new NotImplementedException();
+        }
+
+        public static bool Puzzle6(bool b1, bool b2)
+        {
+            return !b1 || !b2;
+            throw new NotImplementedException();
+        }
+
+        public static bool Puzzle7(bool b1, bool b2)
+        {
+            return !(b1 ^ b2);
+            throw new NotImplementedException();
+        }
+    }
+}
