cf5b9a7441f1c251975b2e9983974a102059e8ec
Inghv <90041989+Inghv@users.noreply.github.com>
2022-07-14 19:11:57 +0300

    LookingForAnArrayElements

---
+++ LookingForAnArrayElement/FloatCounter.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace LookingForArrayElements
+{
+    public static class FloatCounter
+    {
+        /// <summary>
+        /// Searches an array of floats for elements that are in a specified range, and returns the number of occurrences of the elements that matches the range criteria.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of single-precision floating-point numbers.</param>
+        /// <param name="rangeStart">One-dimensional, zero-based <see cref="Array"/> of the range starts.</param>
+        /// <param name="rangeEnd">One-dimensional, zero-based <see cref="Array"/> of the range ends.</param>
+        /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
+        public static int GetFloatsCount(float[]? arrayToSearch, float[]? rangeStart, float[]? rangeEnd)
+        {
+            if (arrayToSearch == null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+            else if (rangeStart == null)
+            {
+                throw new ArgumentNullException(nameof(rangeStart));
+            }
+            else if (rangeEnd == null)
+            {
+                throw new ArgumentNullException(nameof(rangeEnd));
+            }
+            else if (rangeStart.Length == 0 && rangeEnd.Length == 0)
+            {
+                return 0;
+            }
+            else if (rangeStart.Length != rangeEnd.Length)
+            {
+                throw new ArgumentException("rangeStart.Length != rangeEnd.Length", nameof(rangeStart));
+            }
+            else if (rangeStart[^1] > rangeEnd[^1])
+            {
+                throw new ArgumentException("First element of range is greater than last");
+            }
+
+            int count = 0;
+            for (int i = 0; i < arrayToSearch.Length; i++)
+            {
+                for (int j = 0; j < rangeStart.Length; j++)
+                {
+                    if (arrayToSearch[i] <= rangeEnd[j] && arrayToSearch[i] >= rangeStart[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Searches an array of floats for elements that are in a specified range, and returns the number of occurrences of the elements that matches the range criteria.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of single-precision floating-point numbers.</param>
+        /// <param name="rangeStart">One-dimensional, zero-based <see cref="Array"/> of the range starts.</param>
+        /// <param name="rangeEnd">One-dimensional, zero-based <see cref="Array"/> of the range ends.</param>
+        /// <param name="startIndex">The zero-based starting index of the search.</param>
+        /// <param name="count">The number of elements in the section to search.</param>
+        /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
+        public static int GetFloatsCount(float[]? arrayToSearch, float[]? rangeStart, float[]? rangeEnd, int startIndex, int count)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (rangeStart is null)
+            {
+                throw new ArgumentNullException(nameof(rangeStart));
+            }
+
+            if (rangeEnd is null)
+            {
+                throw new ArgumentNullException(nameof(rangeEnd));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (rangeStart.Length != rangeEnd.Length)
+            {
+                throw new ArgumentException("rangeStart.Length != rangeEnd.Length", nameof(rangeStart));
+            }
+
+            if ((startIndex == 0 && rangeStart.Length == 0) || (startIndex == 0 && rangeEnd.Length == 0))
+            {
+                return 0;
+            }
+
+            if (rangeStart.Length > rangeEnd.Length)
+            {
+                throw new ArgumentException("rangeStart.Length > rangeEnd.Length", nameof(rangeStart));
+            }
+
+            if (rangeStart[^1] > rangeEnd[^1])
+            {
+                throw new ArgumentException("First element of range is greater than last");
+            }
+
+            int result = 0;
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                for (int j = 0; j < rangeStart.Length; j++)
+                {
+                    if (arrayToSearch[i] <= rangeEnd[j] && arrayToSearch[i] >= rangeStart[j])
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LookingForAnArrayElement/IntegersCounter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace LookingForArrayElements
+{
+    public static class IntegersCounter
+    {
+        /// <summary>
+        /// Searches an array of integers for elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>, and returns the number of occurrences of the elements.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of integers to search.</param>
+        /// <param name="elementsToSearchFor">One-dimensional, zero-based <see cref="Array"/> that contains integers to search for.</param>
+        /// <returns>The number of occurrences of the elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>.</returns>
+        public static int GetIntegersCount(int[]? arrayToSearch, int[]? elementsToSearchFor)
+        {
+            if (arrayToSearch == null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+            else if (elementsToSearchFor == null)
+            {
+                throw new ArgumentNullException(nameof(elementsToSearchFor));
+            }
+
+            int count = 0;
+            for (int i = 0; i < elementsToSearchFor.Length; i++)
+            {
+                for (int j = 0; j < arrayToSearch.Length; j++)
+                {
+                    if (elementsToSearchFor[i] == arrayToSearch[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Searches an array of integers for elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>, and returns the number of occurrences of the elements withing the range of elements in the <see cref="Array"/> that starts at the specified index and contains the specified number of elements.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of integers to search.</param>
+        /// <param name="elementsToSearchFor">One-dimensional, zero-based <see cref="Array"/> that contains integers to search for.</param>
+        /// <param name="startIndex">The zero-based starting index of the search.</param>
+        /// <param name="count">The number of elements in the section to search.</param>
+        /// <returns>The number of occurrences of the elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>.</returns>
+        public static int GetIntegersCount(int[]? arrayToSearch, int[]? elementsToSearchFor, int startIndex, int count)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (elementsToSearchFor is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (startIndex + count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            if (startIndex == 0 && elementsToSearchFor.Length == 0)
+            {
+                return 0;
+            }
+
+            int result = 0;
+            for (int i = 0; i < elementsToSearchFor.Length; i++)
+            {
+                for (int j = startIndex; j < startIndex + count; j++)
+                {
+                    if (elementsToSearchFor[i] == arrayToSearch[j])
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
