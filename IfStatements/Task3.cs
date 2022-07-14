ecf091f743a94809e87523f374e17fccfd14dafe
Inghv <90041989+Inghv@users.noreply.github.com>
2022-07-14 18:28:22 +0300

    IfStatements

---
+++ IfStatements/Task3.cs
@@ -0,0 +1,28 @@
+﻿namespace IfStatements
+{
+    public static class Task3
+    {
+        public static bool DoSomething1(bool b)
+        {
+            bool result = true;
+            if (b is true)
+            {
+                result = false;
+            }
+
+            return result;
+            throw new NotImplementedException();
+        }
+
+        public static bool DoSomething2(bool b)
+        {
+            if (b)
+            {
+                return false;
+            }
+
+            return true;
+            throw new NotImplementedException();
+        }
+    }
+}
